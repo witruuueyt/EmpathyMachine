9b5adf8f6736af93b818857b7deddce2a46433e2
witruuueyt <926357@swansea.ac.uk>
2023-03-12 01:12:42 +0000

    data collection done

--- Assets/AirtableUnity/Scripts/Model/BaseField.cs
+++
@@ -1,7 +0,0 @@
-namespace AirtableUnity.PX.Model
-{
-    public class BaseField
-    {
-        public long Id { get; set; }
-    }
-}
--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -7,33 +7,32 @@ using TMPro;
 
 public class TextManager : MonoBehaviour
 {
-    public string fileName; //文件名
-    private string filePath; //文件路径
-    public TMP_InputField inputField; //包含玩家输入的TMP Input Field组件
-    public TMP_Text displayText; //要显示文本的TMP Text组件
+    public TMP_InputField inputField; 
+    public TMP_Text waitingText;
+    public TMP_Text nextText; 
+
 
     private void Start()
     {
-        filePath = Path.Combine(Application.persistentDataPath, fileName); //获取文件路径
-        LoadText(); //加载文本
+        
+        string savedText = PlayerPrefs.GetString("savedText", "");
+        waitingText.text = savedText;
     }
 
     public void SaveText()
     {
-        string inputText = inputField.text; //获取TMP Input Field中的文本输入
-        File.WriteAllText(filePath, inputText); //将文本写入文件
+        
+        string inputText = inputField.text;
+        PlayerPrefs.SetString("savedText", inputText);
+        waitingText.text = inputText;
+        StartCoroutine(WaitingCoroutine());
+
     }
 
-    public void LoadText()
+    IEnumerator WaitingCoroutine()
     {
-        if (File.Exists(filePath))
-        {
-            string loadedText = File.ReadAllText(filePath); //从文件读取文本
-            displayText.text = loadedText; //将文本显示在TMP Text组件中
-        }
-        else
-        {
-            Debug.LogError("File not found!"); //如果文件不存在，输出错误信息
-        }
+        yield return new WaitForSeconds(30f);
+        nextText.text = waitingText.text;
+        waitingText.text = "";
     }
 }
